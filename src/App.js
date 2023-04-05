1afddc65e25e8b165148871b3ae4494df74d14e6
Soumyadip1245 <soumyadip20csu214@ncuindia.edu>
2023-04-05 13:06:13 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Website from './Website'
+
+const App = () => {
+  return (
+    <>
+      <Website />
+    </>
+  )
+}
+
+export default App
---
+++ src/Website.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react'
+import './Website.css'
+import Image from './undraw_photo_session_re_c0cp.svg'
+import axios from 'axios';
+
+const Website = () => {
+    const [input, setInput] = useState("");
+    const [image, setImage] = useState(null);
+    const [api, setKey] = useState("")
+    const takeScreenshot = () => {
+        setImage(`https://api.apiflash.com/v1/urltoimage?access_key=${api}&url=${input}`);
+        setInput("");
+        // setKey("")
+    }
+    const Reset = () => {
+        setInput("")
+        setKey("")
+        setImage(null)
+    }
+    return (
+        <>
+            <section>
+                <header>
+                    <h2>ScreenShot</h2>
+                    <nav>
+                        <ul className="nav_links">
+                            <li>
+                                <a href="#">Services</a>
+                            </li>
+                            <li>
+                                <a href="#">Projects</a>
+                            </li>
+                            <li />
+                        </ul>
+                    </nav>
+                    <a href="#" className="cta">
+                        <button>Contact</button>
+                    </a>
+                </header>
+                <div className="custom-shape-divider-bottom-1680597068">
+                    <svg
+                        data-name="Layer 1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1200 120"
+                        preserveAspectRatio="none"
+                    >
+                        <path
+                            d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                            opacity=".25"
+                            className="shape-fill"
+                        />
+                        <path
+                            d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                            opacity=".5"
+                            className="shape-fill"
+                        />
+                        <path
+                            d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                            className="shape-fill"
+                        />
+                    </svg>
+                </div>
+                <main>
+                    <div className="input-class">
+                        <div className="wrapper">
+                            <div className="typing-demo">Enter Details</div>
+                        </div>
+                        <br />
+                        <div className="input-field">
+                            <input type="text" required="" spellCheck="false" value={input} onChange={(e) => setInput(e.target.value)} />
+                            <label>Enter Website</label>
+                        </div>
+                        <div className="input-field">
+                            <input type="password" required="" spellCheck="false" value={api} onChange={(e) => setKey(e.target.value)} />
+                            <label>ApiFlash key </label>
+                        </div>
+                        <div className="btnflex">
+                            <button Style={"margin-right: 2rem"} className="btnn" onClick={takeScreenshot} >Submit</button>
+                            <button className="btnn" onClick={Reset} >Reset</button>
+
+                        </div>
+                    </div>
+                    <div className="image-card">
+                        {image != null ? <img src={image} /> : <img src={Image} alt="" />
+                        }
+                    </div>
+                </main>
+            </section>
+
+        </>
+    )
+}
+
+export default Website
